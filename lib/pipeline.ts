15df3ec2dabd6aafafafb7be76e0206fec5d5f73
phương từ <ptu2747@gmail.com>
2024-08-12 06:33:54 +0000

    Setting up EKS Blueprints deployment pipeline

---
+++ lib/pipeline.ts
@@ -0,0 +1,42 @@
+// lib/pipeline.ts
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+import { KubernetesVersion } from 'aws-cdk-lib/aws-eks';
+
+export default class PipelineConstruct extends Construct {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id)
+
+    const account = props?.env?.account!;
+    const region = props?.env?.region!;
+
+    const blueprint = blueprints.EksBlueprint.builder()
+      .account(account)
+      .region(region)
+      .clusterProvider(
+        new blueprints.GenericClusterProvider({
+          version: KubernetesVersion.V1_29,
+        })
+      )
+      .addOns()
+      .teams();
+
+    blueprints.CodePipelineStack.builder()
+      .name("eks-blueprints-workshop-pipeline")
+      .owner("FromSunNews")
+      .repository({
+        repoUrl: 'my-eks-blueprints',
+        credentialsSecretName: 'eks-workshop-token',
+        targetRevision: 'main'
+      })
+      // WE ADD THE STAGES IN WAVE FROM THE PREVIOUS CODE
+      .wave({
+        id: "envs",
+        stages: [
+          { id: "dev", stackBuilder: blueprint.clone('ap-southeast-1') }
+        ]
+      })
+      .build(scope, id + '-stack', props);
+  }
+}
